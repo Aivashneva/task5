897290ad2164539e363a7567b3fb02df00f9cba5
AIvashneva <Anastasiya.Ivashneva@aplana.com>
2019-05-05 21:09:22 +0300

    complete task5

---
+++ src/task5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class task5 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int number = scan.nextInt();
+        int factor = 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+        factor += 1;
+        System.out.println(number*factor);
+    }
+}
