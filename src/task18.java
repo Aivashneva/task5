de1a4ebad84b3581bda73fa1c6e4b56ce770ad8c
AIvashneva <Anastasiya.Ivashneva@aplana.com>
2019-05-16 12:56:35 +0300

    complete task 18

---
+++ src/task18.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class task18 {
+    public static void main(String[] args) {
+        String s;
+        int lineNumber = 0;
+       //количество строк
+        try (LineNumberReader lineNumberReader = new LineNumberReader(new FileReader("src\\task18.txt")))
+        {
+            while (lineNumberReader.readLine() != null) {
+            lineNumber++;
+            }
+            //System.out.println("Количество строк " + lineNumber + "\n");
+        }
+        catch (IOException exc)
+        {
+            System.out.println("Ошибка" + exc);
+        }
+        // вывод из файла
+        try (BufferedReader br = new BufferedReader (new FileReader("src\\task18.txt")))
+        {
+            while ((s = br.readLine()) != null)
+            {
+                System.out.println(s);
+            }
+        }
+        catch (IOException exc)
+        {
+            System.out.println("Ошибка" + exc);
+        }
+        // перезапись
+        try (FileWriter fw = new FileWriter("src\\task18.txt"))
+        {
+            BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+            System.out.println("\n Введите новые данные: ");
+            do {
+                s = br.readLine();
+                s = s + "\r\n";
+                fw.write(s);
+                lineNumber--;
+            } while (lineNumber != 0);
+        }
+        catch (IOException exc) {
+            System.out.println("Ошибка" + exc);
+        }
+    }
+}
