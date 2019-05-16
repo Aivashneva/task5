f638273c3bdbfdcf92aed1bdc6a74ac4e8fcee25
AIvashneva <Anastasiya.Ivashneva@aplana.com>
2019-05-16 11:50:09 +0300

    complete task 16

---
+++ src/task16.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class task16 {
+    public static void main(String[] args) {
+        String s;
+        try (BufferedReader br = new BufferedReader (new FileReader("src\\myFile.txt")))
+        {
+            while ((s = br.readLine()) != null)
+            {
+                System.out.println(s);
+            }
+        }
+        catch (IOException exc)
+        {
+            System.out.println("Ошибка");
+        }
+    }
+}
