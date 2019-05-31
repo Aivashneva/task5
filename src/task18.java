39bbea50b76155f38394b093b396e1807e78850a
AIvashneva <Anastasiya.Ivashneva@aplana.com>
2019-05-31 09:46:36 +0300

    remarks task 18

--- src/task18.java
+++ src/task18.java
@@ -4,44 +4,38 @@ public class task18 {
     public static void main(String[] args) {
         String s;
         int lineNumber = 0;
-       //количество строк
-        try (LineNumberReader lineNumberReader = new LineNumberReader(new FileReader("src\\task18.txt")))
-        {
+        //количество строк
+        try (LineNumberReader lineNumberReader = new LineNumberReader(new FileReader("src\\task18.txt"))) {
             while (lineNumberReader.readLine() != null) {
-            lineNumber++;
+                lineNumber++;
             }
             //System.out.println("Количество строк " + lineNumber + "\n");
-        }
-        catch (IOException exc)
-        {
+        } catch (IOException exc) {
             System.out.println("Ошибка" + exc);
         }
         // вывод из файла
-        try (BufferedReader br = new BufferedReader (new FileReader("src\\task18.txt")))
-        {
-            while ((s = br.readLine()) != null)
-            {
-                System.out.println(s);
+        if (lineNumber != 0) {
+            try (BufferedReader br = new BufferedReader(new FileReader("src\\task18.txt"))) {
+                while ((s = br.readLine()) != null) {
+                    System.out.println(s);
+                }
+            } catch (IOException exc) {
+                System.out.println("Ошибка" + exc);
             }
-        }
-        catch (IOException exc)
-        {
-            System.out.println("Ошибка" + exc);
-        }
-        // перезапись
-        try (FileWriter fw = new FileWriter("src\\task18.txt"))
-        {
-            BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-            System.out.println("\n Введите новые данные: ");
-            do {
-                s = br.readLine();
-                s = s + "\r\n";
-                fw.write(s);
-                lineNumber--;
-            } while (lineNumber != 0);
-        }
-        catch (IOException exc) {
-            System.out.println("Ошибка" + exc);
-        }
+            // перезапись
+            try (FileWriter fw = new FileWriter("src\\task18.txt")) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("\n Введите новые данные: ");
+                do {
+                    s = br.readLine();
+                    s = s + "\r\n";
+                    fw.write(s);
+                    lineNumber--;
+                } while (lineNumber != 0);
+            } catch (IOException exc) {
+                System.out.println("Ошибка" + exc);
+            }
+        } else
+            System.out.println("Файл пустой!");
     }
 }
